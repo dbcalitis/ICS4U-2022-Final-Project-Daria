d31a645facfe911ddcf6446a21e953680fc25e28
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-12-15 15:00:58 +0000

    layout the website

--- comic-relief/App.js
+++ comic-relief/App.js
@@ -2,12 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import Navbar from './components/Navbar';
+import menuImage from './assets/menu.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <div>
-        <h1>COMIC RELIEF</h1>
+      <div style={styles.topBar}>
+        <img src={menuImage} width="60" height="60" align="left"></img>
+        <h1 style={{textAlign: 'center'}}>COMIC RELIEF</h1>
+      </div>
+      
+      <div style={styles.navBar}>
+          <Navbar></Navbar>
+      </div>
+      
+      <div style={styles.mainPage}>
         <p>Get some Marvel comics to read!</p>
       </div>
       <StatusBar style="auto" />
@@ -17,11 +27,25 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
+  topBar: {
+    position: 'sticky',
+    top: 0,
+    backgroundColor: '#7F669D',
+    padding: '20px',
+  },
+
+  navBar: {
+    backgroundColor: '#BA94D1',
+    fontSize: 25,
+  },
+
+  mainPage: {
+    padding: '20px',
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#7F669D',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#DEBACE',
     fontFamily: 'cursive',
     color: '#FBFACD',
   },
